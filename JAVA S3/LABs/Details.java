b360bf89dcdbda1172e0a985a0cd82bc73b8be68
Roshan Robin <157699910+roshanRobyn@users.noreply.github.com>
2024-09-17 14:15:39 +0530

    Update Details.java

--- JAVA S3/LABs/Details.java
+++ JAVA S3/LABs/Details.java
@@ -22,3 +22,4 @@ class DEETS
 }
 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
     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
+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
